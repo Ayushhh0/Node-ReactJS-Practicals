600b0d5a85497a0125e4e6fa4097f18fa00f20ea
Ayush Tomar <143201847+Ayushhh0@users.noreply.github.com>
2025-09-19 15:13:09 +0530

    Delete Practicle 2/Practicle3 directory

--- Practicle 2/Practicle3/blockingRead.js
+++
@@ -1,9 +0,0 @@
-// blockingRead.js
-const fs = require('fs');
-
-console.log("Starting blocking read...");
-
-const data = fs.readFileSync('sample.txt', 'utf8');
-console.log("File contents:\n", data);
-
-console.log("Blocking read completed.");
